74a3440499948c84c45a70ff5ca29c6f810af6f8
ramismajeed <ramismajeed@gmail.com>
2023-08-20 21:02:52 +0500

    App completed

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,jsonify
+from flask_cors import CORS
 import numpy as np
 import pandas as pd
 import os
-
+from src.components import chatbot
 app= Flask(__name__)
+
+@app.route('/',methods=["POST"])
+def index():
+    data=request.get_json()
+    query=data.get('data')
+    try:
+        chat=chatbot()
+        response=chat.generateresponse(query)
+    except Exception as e:
+        response=str(e)
+        return jsonify({"response":False,"message":response})
+    return jsonify({"response":True,"message":response})
+
+if __name__ == '__main__':
+    app.run(debug=True)        
+    
--- src/components/chatbot.py
+++ src/components/chatbot.py
@@ -7,24 +7,37 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
-    MessagesPlaceholder
-)
+    MessagesPlaceholder)
+from src.utils import *
+from src.exception import CustomException
+from src.logger import logging
 from dotenv.main import load_dotenv
 load_dotenv()
 openai_api_key=os.getenv('OPENAI_API_KEY')
 
-from src.exception import CustomException
-from src.logger import logging
+
 
 class Chatbot:
     def __init__(self):
         self.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
         self.llm=ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=openai_api_key)
-        self.system_msg_template=SystemMessagePromptTemplate.from_template(template="""You Are a chatbot and ineract with the user.""")
+        self.system_msg_template=SystemMessagePromptTemplate.from_template(template="""You Are a chatbot and interact with the user.""")
         self.human_msg_template=HumanMessagePromptTemplate.from_template(template="{input}")
         self.prompt_template=ChatPromptTemplate.from_messages([system_msg_template,MessagesPlaceholder(variable_name='history'),self.human_msg_template])
         self.conversation=ConversationChain(memory=self.buffer_memory,prompt=self.prompt_template,llm=self.llm,verbose=True)
         self.requests=[]
         self.responses=["Hey User How are you"]
+    def generateresponse(self,input):
+        if len(self.requests) > 0:
+            conversation_string=get_conversation_string(requests=self.requests,responses=self.responses)
+            refined_query=query_refiner(input)
+        else:
+            refined_query=input
+        context=findmatch(refined_query)
+        response=self.conversation.predict(input=f"""Context:\n {context} \n\n Query:\n{input} """)
+        self.requests.append(query)
+        self.responses.append(query)
+        return response            
+        
     
     
