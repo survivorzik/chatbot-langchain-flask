6de9ded6c0359c8fe6258246e69ffff7fa111516
ramismajeed <ramismajeed@gmail.com>
2023-08-20 19:50:19 +0500

    Work

--- src/components/chatbot.py
+++ src/components/chatbot.py
@@ -1,15 +1,30 @@
 import os 
 import sys
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+from dotenv.main import load_dotenv
+load_dotenv()
+openai_api_key=os.getenv('OPENAI_API_KEY')
 
-import src.exception import CustomException
+from src.exception import CustomException
 from src.logger import logging
-
-from langchain.llms import OpenAI
-from 
 
 class Chatbot:
     def __init__(self):
-        
-        pass
+        self.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+        self.llm=ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=openai_api_key)
+        self.system_msg_template=SystemMessagePromptTemplate.from_template(template="""You Are a chatbot and ineract with the user.""")
+        self.human_msg_template=HumanMessagePromptTemplate.from_template(template="{input}")
+        self.prompt_template=ChatPromptTemplate.from_messages([system_msg_template,MessagesPlaceholder(variable_name='history'),self.human_msg_template])
+        self.conversation=ConversationChain(memory=self.buffer_memory,prompt=self.prompt_template,llm=self.llm,verbose=True)
+        self.requests=[]
+        self.responses=["Hey User How are you"]
     
     
--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,41 @@ import pinecone
 import openai
 from dotenv.main import load_dotenv
 import os
+load_dotenv()
+
+openai.api_key=os.getenv('OPENAI_API_KEY')
+pineconeapikey=os.getenv('PINECONE_API_KEY')
+model=SentenceTransformer('all-MiniLM-L6-v2')
+pinecone.init(api_key=pineconeapikey,environment='asia-southeast1-gcp-free')
+index=pinecone.Index('chatbot')
+
+def findmatch(input):
+    input_em=model.encode(input).tolist()
+    result=index.query(input_em,top_k=2,includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+'\n'+result['matches'][1]['metadata']['text']
+
+def query_refiner(conversation,query):
+    response=openai.Completion.create(
+        model='ada:ft-personal:refined-query-2023-07-05-21-38-35',
+        prompt=f"""Given the following user query and conversation log, formulate a long detailed question
+        that would be the most relevant to provide the user with an 
+        answer from a knowledge base.\n\n CONVERSATION LOG: \n{conversation} \n\nQuery:{query}\n\n?
+        Refined Query: """,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=['?']
+    )
+    return response['choices'][0]['text']
+
+def get_conversation_string(requests, responses):
+    conversation_string=""
+    for i in range(len(responses)-1):
+        conversation_string+=f"User: {requests[i]}\nBot: {responses[i]}\n"
+    return conversation_string    
 
 
-openai.api_key=
 
 
