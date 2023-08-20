a4a8ad3b54c9d5970b2ca618871df2771da413a8
ramismajeed <ramismajeed@gmail.com>
2023-08-20 17:53:46 +0500

    Structure

---
+++ src/components/chatbot.py
@@ -0,0 +1,15 @@
+import os 
+import sys
+
+import src.exception import CustomException
+from src.logger import logging
+
+from langchain.llms import OpenAI
+from 
+
+class Chatbot:
+    def __init__(self):
+        
+        pass
+    
+    
---
+++ src/utils.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+import pinecone
+import openai
+from dotenv.main import load_dotenv
+import os
+
+
+openai.api_key=
+
+
