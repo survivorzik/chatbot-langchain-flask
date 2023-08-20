cf0e1c57905d33ff35afece5cf5dc18066836368
ramismajeed <ramismajeed@gmail.com>
2023-08-20 21:31:24 +0500

    error

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask,request,render_template,jsonify
 from flask_cors import CORS
-import numpy as np
-import pandas as pd
 import os
-from src.components import chatbot
+from src.components.chatbot import Chatbot
 app= Flask(__name__)
 
-@app.route('/',methods=["POST"])
+@app.route('/data',methods=["POST"])
 def index():
     data=request.get_json()
     query=data.get('data')
+    print(query)
     try:
         chat=chatbot()
         response=chat.generateresponse(query)
     except Exception as e:
         response=str(e)
+        print(response)
         return jsonify({"response":False,"message":response})
     return jsonify({"response":True,"message":response})
 
 if __name__ == '__main__':
-    app.run(debug=True)        
+    app.run(host='0.0.0.0',debug=True,port=5000)        
     
